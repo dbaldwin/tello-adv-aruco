cb00830b9e266f1a1fd738430a1e8a73bc55051a
patrickryan <theyoungsoul@gmail.com>
2020-09-07 19:07:02 -0500

    remote test code from aruco_utils.py

--- droneblocksutils/aruco_utils.py
+++ droneblocksutils/aruco_utils.py
@@ -167,26 +167,3 @@ def detect_distance_from_image_center(image, selected_pt_x, selected_pt_y, show_
     return image, x_distance, y_distance, distance
 
 
-if __name__ == '__main__':
-    import imutils
-    time.sleep(2)
-
-    frame = cv2.imread('../test_data/test_porch_2.JPG')
-    frame = imutils.resize(frame, width=800)
-    image, marker_details = detect_markers_in_image(frame, draw_center=True, draw_reference_corner=True, target_id=None)
-
-    print(frame.shape)
-    if len(marker_details) > 0:
-        for marker_detail in marker_details:
-            center_x, center_y = marker_detail[0]
-            print(center_x, center_y)
-            image, dx, dy,d = detect_distance_from_image_center(image, center_x, center_y, show_detail=True)
-            print(dx,dy,d)
-
-    # Display the resulting frame
-    cv2.imshow('ArUco', image)
-
-    cv2.waitKey(0)
-
-    cv2.destroyWindow('ArUco')
-
--- tests/aruco_marker_test.py
+++ tests/aruco_marker_test.py
@@ -1,27 +1,21 @@
 import cv2
-import time
-from imutils.video import VideoStream
+import imutils
 from droneblocksutils.aruco_utils import detect_markers_in_image, detect_distance_from_image_center
 
 if __name__ == '__main__':
-    vs = VideoStream(src=0).start()
-    time.sleep(2)
+    frame = cv2.imread('../test_data/test_porch_2.JPG')
+    frame = imutils.resize(frame, width=800)
+    image, marker_details = detect_markers_in_image(frame, draw_center=True, draw_reference_corner=True, target_id=None)
 
-    while True:
-        frame = vs.read()
-        image, marker_details = detect_markers_in_image(frame, draw_center=True, draw_reference_corner=True, target_id=5)
+    print(frame.shape)
+    if len(marker_details) > 0:
+        for marker_detail in marker_details:
+            center_x, center_y = marker_detail[0]
+            print(center_x, center_y)
+            image, dx, dy,d = detect_distance_from_image_center(image, center_x, center_y, show_detail=True)
+            print(dx,dy,d)
 
-        if len(marker_details) > 0:
-            for marker_detail in marker_details:
-                center_x, center_y = marker_detail[0]
-                image, dx, dy, d = detect_distance_from_image_center(image, center_x, center_y, show_detail=False)
-                print(dx, dy, d)
+    # Display the resulting frame
+    cv2.imshow('ArUco', image)
 
-        # Display the resulting frame
-        cv2.imshow('ArUco', image)
-
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break
-
-    cv2.destroyWindow('ArUco')
+    cv2.waitKey(0)
