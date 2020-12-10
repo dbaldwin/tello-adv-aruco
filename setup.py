f39f9055c1defc3f8aeb78719ef3d52a01a6708f
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-12-10 17:14:13 -0600

    Dependency updates for Windows compat and Python 3.9

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 
 setup(
-    name='tello_adv_setup',
+    name='tello-adv-aruco',
     version='1.0',
     packages=[''],
     url='',
     license='',
     author='patrick ryan',
     author_email='',
-    description='Tello Advanced Project Development Setup ',
+    description='Tello Advanced ArUco Markers',
     install_requires = [
         # pip install https://github.com/damiafuentes/DJITelloPy/archive/master.zip
-        'opencv-python==4.4.0.42',
-        'opencv-contrib-python==4.4.0.42',
+        'opencv-python==4.4.0.46',
+        'opencv-contrib-python==4.4.0.46',
         'jupyter==1.0.0',
         'imutils==0.5.3',
-        'scipy==1.5.2'
+        'scipy==1.5.4'
     ]
 
 )
